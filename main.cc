f537cbbb52f0fe93328553a261b41fbabea3780d
Roman <romanstrah@mail.ru>
2020-03-25 19:37:03 +0300

    Corrected the appearance.

--- main.cc
+++ main.cc
@@ -4,7 +4,7 @@ int main(int argc, char* argv[])
 {
 	int colorsArray[] = {
 		COLOR_GREEN,	COLOR_BLACK,
-		COLOR_RED,	    COLOR_BLACK,
+		COLOR_RED,	COLOR_BLACK,
 		COLOR_BLUE, 	COLOR_BLACK,
 		COLOR_CYAN,  	COLOR_BLACK,
 		COLOR_YELLOW,	COLOR_BLACK,
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 		COLOR_WHITE,	COLOR_BLACK 
 	};
 	MainController controller;
-	//controller.readParameters();
+	controller.readParameters();
 	controller.ncursesStart(0, 1, 0, 1, 7, colorsArray);
 	controller.drawingStart();
 }
